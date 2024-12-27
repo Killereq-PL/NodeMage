32053b065f252d5834d84c856bba29698276ae3c
Killereq-PL <adam.kotek33@gmail.com>
2024-12-27 18:48:16 +0100

    futureproofing

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import dearpygui.dearpygui as dpg
 from webbrowser import open_new_tab
 from os import _exit
+from nodes import Nodes
 
 class MainApp:
     def __init__(self):
         self.add_shortcut = dpg.mvKey_Spacebar
         self.add_menu_exists = False
         self.settings_menu_exists = False
-        self.node_library = {"Basic": ["Add", "Multiply", "Screen", "ConvertToGrayscale", "MapColor"], 
+        self.node_library = {"Basic": ["Add", "Multiply", "Screen", "ConvertToGrayscale", "MapColor", "ChannelSplit", "ChannelJoin"], 
                             "Noise": ["Perlin", "Simplex", "Voronoi", "Worley"], 
                             "Filter": ["Blur", "Sharpen", "EdgeDetection", "Emboss", "GaussianBlur"], 
                             "Transform": ["Rotate", "Scale", "Translate", "Flip", "Mirror"], 
-                            "Input": ["Texture"], 
+                            "Random": ["Seed", "Range"],
+                            "Input": ["Texture", "Color"], 
                             "Output": ["Preview", "Export"]}
         dpg.create_context()
         
@@ -20,7 +22,7 @@ class MainApp:
         
         dpg.create_viewport(title='NodeMage', width=1280, height=720)
         
-        with dpg.window(tag="mainWindow", no_title_bar=True, no_move=True, no_resize=True, no_collapse=True, no_close=True):
+        with dpg.window(tag="main_window", no_title_bar=True, no_move=True, no_resize=True, no_collapse=True, no_close=True):
             with dpg.menu_bar():
                 with dpg.menu(label="File"):
                     dpg.add_menu_item(label="New")
@@ -34,7 +36,7 @@ class MainApp:
                     dpg.add_menu_item(label="Settings", callback=self.create_settings_menu)
                 dpg.add_menu_item(label="Help", callback=self.help)
                 dpg.add_menu_item(label="Add (Spacebar)", callback=self.create_add_menu)
-            with dpg.node_editor(callback=self.link_callback, delink_callback=self.delink_callback):
+            with dpg.node_editor(tag="node_editor", callback=self.link_callback, delink_callback=self.delink_callback):
                 with dpg.node(label="Node 1"):
                     with dpg.node_attribute(label="Node A1"):
                         dpg.add_input_float(label="F1", width=150)
@@ -51,7 +53,7 @@ class MainApp:
         dpg.bind_font(default_font)
         dpg.setup_dearpygui()
         dpg.show_viewport()
-        dpg.set_primary_window("mainWindow", True)
+        dpg.set_primary_window("main_window", True)
         while dpg.is_dearpygui_running():
             self.update()
         dpg.destroy_context()
@@ -69,7 +71,7 @@ class MainApp:
         # app_data -> link_id
         dpg.delete_item(app_data)
     
-    def add_menu_callback(self, sender, app_data, user_data):
+    def add_menu_callback(sender, app_data, user_data):
         print(f"sender is: {sender}")
         print(f"app_data is: {app_data}")
         print(f"user_data is: {user_data}")
@@ -77,13 +79,16 @@ class MainApp:
     def delete_add_menu(self):
         dpg.delete_item(item="add_menu_window")
 
-    def create_add_menu(self):
+    def create_add_menu(self, isOpenedFromMenu: bool = True):
         if not self.add_menu_exists:
-            with dpg.window(tag="add_menu_window", no_collapse=True, no_move=True, no_resize=True, no_title_bar=True, no_close=True, width=200, height=200, pos=(dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]+10)):
+            pos = (dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]+10) if isOpenedFromMenu else (dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1])
+            with dpg.window(tag="add_menu_window", no_collapse=True, no_move=True, no_resize=True, no_title_bar=True, no_close=True, width=200, height=200, pos=pos):
                 for i in self.node_library.keys():
                     with dpg.menu(label=i):
                         for j in self.node_library[i]:
-                            dpg.add_menu_item(label=''.join(map(lambda x: x if x.islower() else " "+x, j)), callback=self.add_menu_callback, user_data=f'{str(i)}_{str(j)}'.lower())
+                            mitem = dpg.add_menu_item(label=''.join(map(lambda x: x if x.islower() else " "+x, j)), callback=self.add_menu_callback, user_data=f'{str(i)}_{str(j)}'.lower())
+                            with dpg.tooltip(mitem):
+                                dpg.add_text(f'{str(i)}_{str(j)}'.lower())
             self.add_menu_exists = True
 
     def delete_settings_menu(self):
@@ -91,8 +96,8 @@ class MainApp:
 
     def create_settings_menu(self):
         if not self.settings_menu_exists:
-            with dpg.window(label="Settings", tag="settings_menu_window", no_collapse=True, width=800, height=600, pos=(dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]+10)):
-                dpg.add_text(label="Nothing here yet")
+            with dpg.window(label="Settings", tag="settings_menu_window", no_collapse=True, width=800, height=600, pos=(dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]-10)):
+                dpg.add_text("Nothing here yet")
             self.settings_menu_exists = True
 
     def help(self):
@@ -108,7 +113,7 @@ class MainApp:
                 self.delete_settings_menu()
                 self.settings_menu_exists = False
         if dpg.is_key_pressed(self.add_shortcut):
-            self.create_add_menu()
+            self.create_add_menu(False)
         dpg.render_dearpygui_frame()
 
 if __name__ == "__main__":
--- nodes.py
+++ nodes.py
@@ -1,6 +1,90 @@
 class Nodes:
-    def __init__(self, name):
+    def __init__(self):
         pass
 
     def add_node(self, name):
+        getattr(self, f'add_{name}')()
+
+    def add_basic_add(self):
         pass
+
+    def add_basic_multiply(self):
+        pass
+
+    def add_basic_screen(self):  
+        pass
+
+    def add_basic_converttograyscale(self):
+        pass
+
+    def add_basic_mapcolor(self):
+        pass
+
+    def add_basic_channelsplit(self):
+        pass
+
+    def add_basic_channeljoin(self):
+        pass
+
+    def add_noise_perlin(self):
+        pass
+
+    def add_noise_simplex(self):
+        pass
+
+    def add_noise_voronoi(self):
+        pass
+
+    def add_noise_worley(self):
+        pass
+
+    def add_filter_blur(self):
+        pass
+
+    def add_filter_sharpen(self):
+        pass
+
+    def add_filter_edgedetection(self):
+        pass
+
+    def add_filter_emboss(self):
+        pass
+
+    def add_filter_gaussianblur(self):
+        pass
+
+    def add_transform_rotate(self):
+        pass
+
+    def add_transform_scale(self):
+        pass
+
+    def add_transform_translate(self):
+        pass
+
+    def add_transform_flip(self):
+        pass
+
+    def add_transform_mirror(self):
+        pass
+
+    def add_random_seed(self):
+        pass
+
+    def add_random_range(self):
+        pass
+
+    def add_input_texture(self):
+        pass
+
+    def add_input_color(self):
+        pass
+
+    def add_output_preview(self):
+        pass
+
+    def add_output_export(self):
+        pass
+
+if __name__ == "__main__":
+    nodes = Nodes()
