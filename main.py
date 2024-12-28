a84b0ab01d758dabca61f18318825b8723a3d11f
Killereq-PL <adam.kotek33@gmail.com>
2024-12-28 17:39:09 +0100

    Finally fixed a lot of stuff

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class MainApp:
         self.add_shortcut = dpg.mvKey_Spacebar
         self.add_menu_exists = False
         self.settings_menu_exists = False
+        self.is_file_unsaved = True
+        self.current_filename = "Untitled"
         self.nodes = Nodes()
         self.node_library = {"Basic": ["Add", "Multiply", "Blend", "ConvertToGrayscale", "MapColor", "ChannelSplit", "ChannelJoin"], 
                             "Noise": ["Perlin", "Simplex", "Voronoi", "Worley"], 
@@ -16,27 +18,31 @@ class MainApp:
                             "Random": ["Seed", "Range"],
                             "Input": ["Texture", "Color"], 
                             "Output": ["Preview", "Export"]}
+        self.clipboard = []
         dpg.create_context()
         
         with dpg.font_registry():
             default_font = dpg.add_font("Roboto-Regular.ttf", 16)
         
         dpg.create_viewport(title='NodeMage', width=1280, height=720)
+        dpg.set_viewport_title(f'NodeMage - {self.current_filename}{"*" if self.is_file_unsaved else ""}')
         
         with dpg.window(tag="main_window", no_title_bar=True, no_move=True, no_resize=True, no_collapse=True, no_close=True):
             with dpg.menu_bar():
                 with dpg.menu(label="File"):
-                    dpg.add_menu_item(label="New")
-                    dpg.add_menu_item(label="Save")
-                    dpg.add_menu_item(label="Save As")
-                    dpg.add_menu_item(label="Open")
+                    dpg.add_menu_item(label="New", callback=self.new)
+                    dpg.add_menu_item(label="Save", callback=self.save)
+                    dpg.add_menu_item(label="Save As", callback=self.save_as)
+                    dpg.add_menu_item(label="Open", callback=self.open)
                     dpg.add_menu_item(label="Exit", callback=self.exit)
                 with dpg.menu(label="Edit"):
-                    dpg.add_menu_item(label="Copy")
-                    dpg.add_menu_item(label="Paste")
+                    dpg.add_menu_item(label="Copy", callback=self.copy)
+                    dpg.add_menu_item(label="Paste", callback=self.paste)
                     dpg.add_menu_item(label="Settings", callback=self.create_settings_menu)
+                    
                 dpg.add_menu_item(label="Help", callback=self.help)
                 dpg.add_menu_item(label="Add (Spacebar)", callback=self.create_add_menu)
+            
             dpg.add_node_editor(tag="node_editor", callback=self.link_callback, delink_callback=self.delink_callback, minimap=True)
         dpg.bind_font(default_font)
         dpg.setup_dearpygui()
@@ -45,6 +51,30 @@ class MainApp:
         while dpg.is_dearpygui_running():
             self.update()
         dpg.destroy_context()
+    
+    def new(self):
+        self.is_file_unsaved = True
+        self.current_filename = "Untitled"
+    
+    def save(self):
+        self.is_file_unsaved = False
+    
+    def save_as(self):
+        self.is_file_unsaved = False
+    
+    def open(self):
+        self.is_file_unsaved = False
+    
+    def copy(self):
+        if len(dpg.get_selected_nodes("node_editor")) > 0:
+            self.clipboard = []
+            for x in dpg.get_selected_nodes("node_editor"):
+                self.clipboard.append(dpg.get_item_user_data(x))
+    
+    def paste(self):
+        if self.clipboard:
+            for node in self.clipboard:
+                self.nodes.add_node(node)
 
     def exit(self):
         _exit(0)
@@ -60,17 +90,20 @@ class MainApp:
         dpg.delete_item(app_data)
     
     def add_menu_callback(self, sender, app_data, user_data):
-        print(f"sender is: {sender}")
-        print(f"app_data is: {app_data}")
-        print(f"user_data is: {user_data}")
+        print(f"Creating {user_data}")
         self.nodes.add_node(user_data)
+        self.delete_add_menu()
 
     def delete_add_menu(self):
         dpg.delete_item(item="add_menu_window")
+        self.add_menu_exists = False
 
     def create_add_menu(self, isOpenedFromMenu: bool = True):
         if not self.add_menu_exists:
-            pos = (dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]+10) if isOpenedFromMenu else (dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1])
+            if isOpenedFromMenu:
+                pos = (dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]+10)
+            else:
+                pos = (dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1])
             with dpg.window(tag="add_menu_window", no_collapse=True, no_move=True, no_resize=True, no_title_bar=True, no_close=True, width=200, height=200, pos=pos):
                 for i in self.node_library.keys():
                     with dpg.menu(label=i):
@@ -84,10 +117,11 @@ class MainApp:
 
     def delete_settings_menu(self):
         dpg.delete_item(item="settings_menu_window")
+        self.settings_menu_exists = False
 
     def create_settings_menu(self):
         if not self.settings_menu_exists:
-            with dpg.window(label="Settings", tag="settings_menu_window", no_collapse=True, width=800, height=600, pos=(dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]-10)):
+            with dpg.window(label="Settings", tag="settings_menu_window", no_collapse=True, width=800, height=600, pos=(dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]-20)):
                 dpg.add_text("Nothing here yet")
             self.settings_menu_exists = True
 
@@ -96,13 +130,11 @@ class MainApp:
 
     def update(self):
         if self.add_menu_exists:
-            if (not dpg.is_item_hovered("add_menu_window") and dpg.is_mouse_button_clicked(0)) or dpg.is_mouse_button_clicked(1) or dpg.is_key_pressed(dpg.mvKey_Escape):
+            if dpg.is_mouse_button_clicked(1) or dpg.is_key_pressed(dpg.mvKey_Escape):
                 self.delete_add_menu()
-                self.add_menu_exists = False
         if self.settings_menu_exists:
             if dpg.is_key_pressed(dpg.mvKey_Escape):
                 self.delete_settings_menu()
-                self.settings_menu_exists = False
         if dpg.is_key_pressed(self.add_shortcut):
             self.create_add_menu(False)
         if dpg.is_key_pressed(dpg.mvKey_F1):
@@ -110,8 +142,8 @@ class MainApp:
         if dpg.is_key_pressed(dpg.mvKey_Delete):
             for item in dpg.get_selected_nodes("node_editor"):
                 self.nodes.delete_node(item)
-        if dpg.is_key_pressed(dpg.mvKey_Add):
-            self.nodes.add_node("basic_add")
+            for item in dpg.get_selected_links("node_editor"):
+                dpg.delete_item(item)
         dpg.render_dearpygui_frame()
 
 if __name__ == "__main__":
--- nodes.py
+++ nodes.py
@@ -46,6 +46,8 @@ class Nodes:
                 dpg.add_text("Image A")
             with dpg.node_attribute(label="ImageB"):
                 dpg.add_text("Image B")
+            with dpg.node_attribute(label="Factor"):
+                dpg.add_input_float(width=100, label="Factor", max_value=1.0, min_value=0.0, max_clamped=True, min_clamped=True)
             
             with dpg.node_attribute(label="OutputImage", attribute_type=dpg.mvNode_Attr_Output):
                 dpg.add_text("Output Image")
@@ -62,13 +64,48 @@ class Nodes:
             self.nodes[node] = id
 
     def add_basic_mapcolor(self):
-        pass
+        id = "basic_mapcolor"
+        with dpg.node(label="Map Color", parent="node_editor", pos=(dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]), user_data=id) as node:
+            with dpg.node_attribute(label="Image"):
+                dpg.add_text("Image")
+            with dpg.node_attribute(label="RGB"):
+                dpg.add_color_edit(width=200, label="Color", default_value=(255, 255, 255, 255))
+            
+            with dpg.node_attribute(label="OutputImage", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_text("Output Image")
+            self.nodes[node] = id
 
     def add_basic_channelsplit(self):
-        pass
+        id = "basic_channelsplit"
+        with dpg.node(label="Channel Split", parent="node_editor", pos=(dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]), user_data=id) as node:
+            with dpg.node_attribute(label="Image"):
+                dpg.add_text("Image")
+            
+            with dpg.node_attribute(label="R", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_text("Red")
+            with dpg.node_attribute(label="G", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_text("Green")
+            with dpg.node_attribute(label="B", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_text("Blue")
+            with dpg.node_attribute(label="A", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_text("Alpha")
+            self.nodes[node] = id
 
     def add_basic_channeljoin(self):
-        pass
+        id = "basic_channeljoin"
+        with dpg.node(label="Channel Join", parent="node_editor", pos=(dpg.get_mouse_pos(local=False)[0], dpg.get_mouse_pos(local=False)[1]), user_data=id) as node:
+            with dpg.node_attribute(label="R"):
+                dpg.add_text("Red")
+            with dpg.node_attribute(label="G"):
+                dpg.add_text("Green")
+            with dpg.node_attribute(label="B"):
+                dpg.add_text("Blue")
+            with dpg.node_attribute(label="A"):
+                dpg.add_text("Alpha")
+            
+            with dpg.node_attribute(label="OutputImage", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_text("Output Image")
+            self.nodes[node] = id
 
     def add_noise_perlin(self):
         pass
